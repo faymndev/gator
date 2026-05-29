1cb716a1fa658a972eea6d476433caf3e5fd3f34
faymndev <faymndev@gmail.com>
2026-05-28 22:29:05 -0700

    oops - swapped state and command files

--- internal/command/state.go
+++ internal/command/state.go
@@ -1,44 +1,11 @@
 package command
 
-import "fmt"
+import (
+	config "github.com/faymndev/gator/internal"
+	"github.com/faymndev/gator/internal/database"
+)
 
-type Command struct {
-	Name string
-	Args []string
-}
-
-func NewCommand(args []string) Command {
-	cmd := Command{}
-	if len(args) >= 2 {
-		cmd.Name = (args)[1]
-	}
-	if len(args) >= 3 {
-		cmd.Args = args[2:]
-	}
-
-	return cmd
-}
-
-type Commands struct {
-	Registry map[string]HandleCommand
-}
-
-type HandleCommand func(*State, Command) error
-
-func NewCommands() *Commands {
-	return &Commands{
-		Registry: make(map[string]HandleCommand),
-	}
-}
-
-func (c *Commands) Register(name string, handle HandleCommand) {
-	c.Registry[name] = handle
-}
-
-func (c *Commands) Run(s *State, cmd Command) error {
-	handle, ok := c.Registry[cmd.Name]
-	if !ok {
-		return fmt.Errorf("command %q does not exist", cmd.Name)
-	}
-	return handle(s, cmd)
+type State struct {
+	Cfg *config.Config
+	Db  *database.Queries
 }
